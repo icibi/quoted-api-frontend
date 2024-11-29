3a5f456c4448ae2415245c6c22e4962f6cb829ff
Zhenqiao Wang <zwang279@my.centennialcollege.ca>
2024-11-28 19:39:09 -0500

    added admin components and updated quotes

--- frontend/src/components/Favourites.js
+++ frontend/src/components/Favourites.js
@@ -5,7 +5,7 @@ import { useNavigate, Link } from "react-router-dom";
 
 const Favourites = () => {
   const [quotes, setQuotes] = useState([])
-  const [quotesCopy, setQuotesCopy]= useState([])
+  const [quotesCopy, setQuotesCopy] = useState([])
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null);
   const [query, setQuery] = useState('')
@@ -13,19 +13,18 @@ const Favourites = () => {
   const [favId, setFavId] = useState([])
   const apiURL = 'http://localhost:5163/api'
   const nav = useNavigate()
-  
+
 
   // Fetch user's fav quotes from the backend API
   useEffect(() => {
     const user = localStorage.getItem('user')
-    if(!user)
-    {
+    if (!user) {
       nav('/login')
       return
     }
 
     const userId = JSON.parse(user).uid
-    
+
     const fetchQuotes = async () => {
       try {
         const response = await axios.get(`${apiURL}/Favourites?Uid=${userId}`);
@@ -41,68 +40,71 @@ const Favourites = () => {
     fetchQuotes();
   }, [nav]);
 
+  //handle filter change
   const handleSearchChange = (e) => {
     setQuery(e.target.value)
   }
 
-  const handleSearch = async () => 
-  {
-    if(!query.trim()) return
+  //handle filter
+  const handleSearch = async () => {
+    if (!query.trim()) return
 
     setLoading(false)
     const userId = JSON.parse(localStorage.getItem('user')).uid
-    try{
+    try {
       const response = await axios.get(`${apiURL}/Favourites?Uid=${userId}&tag=${query}`);
-        setQuotes(response.data);
-        setLoading(false);
+      setQuotes(response.data);
+      setLoading(false);
     } catch (err) {
       setError(err.message);
       setLoading(false);
     }
   }
 
-  const handleClearFilter = () => 
-  {
+  //clear filter
+  const handleClearFilter = () => {
     setQuery('')
     setQuotes(quotesCopy)
   }
 
+  //handle tag change
   const handleQuoteTagsChange = (e) => {
     setNewTags(e.target.value)
   }
 
-  const handleEditTags = async (quote) =>
-  {
+  //handle save tag changes
+  const handleEditTags = async (quote) => {
     setFavId(quote.fid)
     setNewTags(quote.tags)
   }
 
+  //handle patch tag changes
   const handleSaveTagUpdates = async () => {
     const userId = JSON.parse(localStorage.getItem('user')).uid
-    try{
-      await axios.patch(`${apiURL}/Favourites/patch-tags/${favId}?Uid=${userId}&tags=${newTags}` 
+    try {
+      await axios.patch(`${apiURL}/Favourites/patch-tags/${favId}?Uid=${userId}&tags=${newTags}`
       )
       setFavId([])
       const response = await axios.get(`${apiURL}/Favourites?Uid=${userId}`);
-        setQuotes(response.data);
-        setQuotesCopy(response.data)
-        setLoading(false);
-    
+      setQuotes(response.data);
+      setQuotesCopy(response.data)
+      setLoading(false);
+
     } catch (err) {
       setError(err.message);
       setLoading(false);
     }
   }
 
-  const handleDeleteFavQuote = async (fid) =>
-  {
+  //handle delete
+  const handleDeleteFavQuote = async (fid) => {
     const userId = JSON.parse(localStorage.getItem('user')).uid
-    try{
+    try {
       await axios.delete(`${apiURL}/Favourites/delete/${fid}?Uid=${userId}`)
       const response = await axios.get(`${apiURL}/Favourites?Uid=${userId}`)
-        setQuotes(response.data)
-        setQuotesCopy(response.data)
-        setLoading(false)
+      setQuotes(response.data)
+      setQuotesCopy(response.data)
+      setLoading(false)
     } catch (err) {
       setError(err.message)
       setLoading(false)
@@ -117,57 +119,57 @@ const Favourites = () => {
 
   return (
     <div className="page-container">
-    <div className="quotes-container">
-      <h1>Favourite Quotes</h1>
-      <div className="filter-container">
-        <input type="text" placeholder="Filter by Tag..." value={query} onChange={handleSearchChange}></input>
-        <button onClick={handleSearch}>Filter</button>
-        <button onClick={handleClearFilter}>Clear Filter</button>
-      </div>
-      {quotes.length > 0 ? (
-        <table border="1" style={{ width: "100%", borderCollapse: "collapse" }}>
-          <thead>
-            <tr>                         
-              <th>Quote</th>
-              <th>By</th>
-              <th>Tags</th>
-              <th>Edit Tags</th>
-              <th>Delete</th>
-            </tr>
-          </thead>
-          <tbody>
-            {quotes.map((quote, index) => (
-              <tr key={index}>    
-                <td><Link to={`/favquote/${quote.fid}`}>{quote.quoteContents}</Link></td>         
-                
-                <td>{quote.quoteBy}</td>
-                
-                <td className="edit">
-                  {favId === quote.fid ? (
-                    <div className="edit-tag">
-                    <input type="text" value={newTags} onChange={handleQuoteTagsChange}></input>
-                    <button onClick={handleSaveTagUpdates}>Save</button>
-                    </div>
-                  ) : (
-                    quote.tags
-                  )}
-                </td>
-                <td className="edit">
-                  {favId !== quote.fid && (
-                    <button onClick={() => handleEditTags(quote)} className="editBtn">Edit</button>
-                  )}
-                </td>
-                <td className="delete">
-                  <button onClick={() => handleDeleteFavQuote(quote.fid)} className="deleteBtn">Delete</button>
-                </td>
+      <div className="quotes-container">
+        <h1>Favourite Quotes</h1>
+        <div className="filter-container">
+          <input type="text" placeholder="Filter by Tag..." value={query} onChange={handleSearchChange}></input>
+          <button onClick={handleSearch}>Filter</button>
+          <button onClick={handleClearFilter}>Clear Filter</button>
+        </div>
+        {quotes.length > 0 ? (
+          <table border="1" style={{ width: "100%", borderCollapse: "collapse" }}>
+            <thead>
+              <tr>
+                <th>Quote</th>
+                <th>By</th>
+                <th>Tags</th>
+                <th>Edit Tags</th>
+                <th>Delete</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      ) : (
-        <div>No favourite quotes added.</div>
-      )}
-    </div>
+            </thead>
+            <tbody>
+              {quotes.map((quote, index) => (
+                <tr key={index}>
+                  <td><Link to={`/favquote/${quote.fid}`}>{quote.quoteContents}</Link></td>
+
+                  <td>{quote.quoteBy}</td>
+
+                  <td className="edit">
+                    {favId === quote.fid ? (
+                      <div className="edit-tag">
+                        <input type="text" value={newTags} onChange={handleQuoteTagsChange}></input>
+                        <button className="editBtn" onClick={handleSaveTagUpdates}>Save</button>
+                      </div>
+                    ) : (
+                      quote.tags
+                    )}
+                  </td>
+                  <td className="edit">
+                    {favId !== quote.fid && (
+                      <button onClick={() => handleEditTags(quote)} className="editBtn">Edit</button>
+                    )}
+                  </td>
+                  <td className="delete">
+                    <button onClick={() => handleDeleteFavQuote(quote.fid)} className="deleteBtn">Delete</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <div>No favourite quotes added.</div>
+        )}
+      </div>
     </div>
   );
 };
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -8,18 +8,20 @@ const Header = () => {
   const [role, setRole] = useState('')
   const nav = useNavigate()
 
+  //get user and set
   useEffect(() => {
     const user = localStorage.getItem('user')
-    if(user){
+    if (user) {
       const storedUserDetails = JSON.parse(localStorage.getItem('user'))
-      if(storedUserDetails) {
+      if (storedUserDetails) {
         setUserDetails(storedUserDetails)
         setIsLoggedIn(true)
         setRole(storedUserDetails.userRole)
       }
     }
   }, [])
-  
+
+  //handle log out
   const handleLogOut = () => {
     localStorage.removeItem('user')
     setUserDetails(null)
@@ -28,35 +30,35 @@ const Header = () => {
     window.location.reload()
   }
 
-    return (
-        <header>
-        <nav className="navbar">
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/favourites">Favourites</Link></li>
+  return (
+    <header>
+      <nav className="navbar">
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/favourites">Favourites</Link></li>
 
-            {role === 'admin' ? (
-              <li><Link to="/admin">Admin Functions</Link></li>
-            ) : (
-              <div></div>
-            )}
-            </ul>
-            {isLoggedIn ? (
-              <div className='welcome-container'>
-                <span className='welcome'>Welcome, {userDetails.username}</span>
-                <button className='logout-button' onClick={handleLogOut}>Log Out</button>
-              </div>
-            ) : (
-              <ul>
-              <li><Link to="/signup">Sign up</Link></li>
-              <li><Link to="/login">Login</Link></li>
-              </ul>
-            )}
-            
-        </nav>
-      </header>
-    );
-  };
-  
+          {role === 'admin' ? (
+            <li><Link to="/admin">Admin Functions</Link></li>
+          ) : (
+            <div></div>
+          )}
+        </ul>
+        {isLoggedIn ? (
+          <div className='welcome-container'>
+            <Link to={"/update-acc"} className='welcome'>Welcome, {userDetails.username}</Link>
+            <button className='logout-button' onClick={handleLogOut}>Log Out</button>
+          </div>
+        ) : (
+          <ul>
+            <li><Link to="/signup">Sign up</Link></li>
+            <li><Link to="/login">Login</Link></li>
+          </ul>
+        )}
+
+      </nav>
+    </header>
+  );
+};
+
 export default Header;
 
